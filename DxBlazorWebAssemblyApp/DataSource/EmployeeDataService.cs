79deca88b462ff1c4954193b97088c729d5523f8
jayakumarvinayagam2023 <150107112+jayakumarvinayagam2023@users.noreply.github.com>
2024-02-15 09:32:37 +0530

    Add project files.

---
+++ DxBlazorWebAssemblyApp/DataSource/EmployeeDataService.cs
@@ -0,0 +1,48 @@
+﻿namespace DxBlazorWebAssemblyApp
+{
+    public record Employee
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        public string? Department { get; init; }
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
+        public bool ReplaceFile { get; set; } = true;
+    }
+
+    public interface IEmployeeDataService
+    {
+        Task<IEnumerable<Employee>> GetEmployees();
+    }
+
+    public class EmployeeDataService : IEmployeeDataService
+    {
+        private readonly IList<Employee>? _employee = null;
+        public EmployeeDataService()
+        {
+                _employee = new List<Employee>() {
+                    new(){
+                        Id = 101,
+                        FirstName = "Joydip",
+                        MiddleName = "AAA",
+                        LastName = "Kanjilal",
+                        Department = "Development",
+                        Address = "Banjara Hills",
+                        Phone = "1234567890"
+                    },
+                    new(){
+                        Id = 102,
+                        FirstName = "Jaya",
+                        MiddleName = "Kumar",
+                        LastName = "Vinayagam",
+                        Department = "Development",
+                        Address = "Banjara Hills",
+                        Phone = "1234567890"
+                    }
+                };
+        }
+        public async Task<IEnumerable<Employee>> GetEmployees() => await Task.FromResult(_employee!);
+    }
+}
---
+++ DxBlazorWebAssemblyApp/PageBase/EmployeeMainBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace DxBlazorWebAssemblyApp
+{
+    public class EmployeeMainBase : ComponentBase
+    {
+        [Inject]
+        public IEnumerable<Employee>? Employees { get; set; }
+
+        [Inject]
+        public IEmployeeDataService? EmployeeDataService { get; set; }
+
+        protected IReadOnlyList<object>? SelectedDataItems { get; set; }
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Employees = EmployeeDataService?.GetEmployees().Result;
+            SelectedDataItems = new List<object>();
+        }
+
+        protected Task GenerateScanCover()
+        {
+            var selected = SelectedDataItems.Select(x => (x as Employee)).ToList();
+            return Task.CompletedTask;
+        }
+    }
+}
